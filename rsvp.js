649b38b86b5b20ff0fa60cd58e1f51ce18d18f4a
mondragonkurt-cloud <mondragonkurt@gmail.com>
2026-08-18 14:41:34 +0800

    Add files via upload

---
+++ rsvp.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const form = document.querySelector("form");
+
+    form.addEventListener("submit", function () {
+
+        alert(
+            "Thank you for your RSVP! We look forward to celebrating with you."
+        );
+
+    });
+
+});
